a18d622698351747213980ad1f0b1d66333cbb0d
Andrey Timoshenkov <andreytmshnkv@gmail.com>
2025-02-22 11:29:39 +0300

    finish assignment

---
+++ src/app/movie-item/movie-item.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { Movie } from "../model/movie.model";
+import { CurrencyPipe } from "@angular/common";
+
+
+@Component({
+  selector: 'app-movie-item',
+  template: `
+    <div class="movie-item">
+      <div>
+        <h4>{{ movie.title }}</h4>
+        <small class="subtitle">
+          <span>Release date: {{ movie.release_date }}</span>
+          <span>Budget: {{ movie.budget | currency: 'USD' : '$' :'1.0' }} million</span>
+          <span>Duration: {{ movie.duration }} min</span>
+
+        </small>
+      </div>
+      <button>Details</button>
+    </div>
+  `,
+  standalone: true,
+  imports: [
+    CurrencyPipe
+  ],
+  styleUrls: ['movie-item.component.scss']
+})
+export class MovieItemComponent {
+  @Input() movie: Movie;
+
+}
+
